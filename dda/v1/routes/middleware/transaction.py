30c772d7564e3ea6c7c32d808bd381ddbdb6e436
Austin Graham <austingraham731@gmail.com>
2025-03-01 15:39:30 -0800

    fix style and typing

--- dda/v1/routes/middleware/transaction.py
+++ dda/v1/routes/middleware/transaction.py
@@ -1,18 +1,19 @@
 import logging
 import time
 import uuid
-from typing import Callable
 from django.http import HttpRequest
 from django.http import HttpResponse
+from dda.v1.routes.http import APIRequest
 from dda.v1.routes.http import APIRequestState
+from dda.v1.routes.middleware.types import ResponseProcessor
 
 
 logger = logging.getLogger("dda")
 
 
-def transaction_middleware(get_response: Callable[[HttpRequest], HttpResponse]):
+def transaction_middleware(get_response: ResponseProcessor[HttpRequest]) -> ResponseProcessor[APIRequest]:
     """Middleware to initialize a transaction and time a request."""
-    def middleware(request: HttpRequest) -> HttpResponse:
+    def middleware(request: APIRequest) -> HttpResponse:
         request.state = APIRequestState(tid=uuid.uuid4())
         logger.info("REQUEST START", extra=request.state.dict())
 
@@ -21,7 +22,7 @@ def transaction_middleware(get_response: Callable[[HttpRequest], HttpResponse]):
         end_time = time.time()
 
         total_time_ms = round((end_time - start_time) * 1000)
-        logger.info(f"REQUEST END", extra=request.state.dict())
+        logger.info("REQUEST END", extra=request.state.dict())
         logger.info(f"Total request time {total_time_ms}ms", extra=request.state.dict())
         return response
     return middleware
