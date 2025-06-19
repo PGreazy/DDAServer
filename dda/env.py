68413d679e8ab5d7e98aacbd4d54047d85e0a3e6
Austin Graham <austingraham731@gmail.com>
2025-06-19 00:26:18 -0700

    add databse context to helm chart

--- dda/env.py
+++ dda/env.py
@@ -24,3 +24,20 @@ class Env(Enum):
         if env_string is None:
             raise ValueError("DJANGO_ENV must be set.")
         return Env(env_string)
+
+
+def set_database_url() -> None:
+    """
+    Sets the DATABASE_URL environment variable to the construction
+    of a connection string from separated variables if the DATABASE_URL
+    variable is not already provided.
+    """
+    if os.environ.get("DATABASE_URL"):
+        return
+
+    db_host = os.environ.get("DB_HOST", "")
+    db_port = os.environ.get("DB_PORT", "")
+    db_user = os.environ.get("DB_USER", "")
+    db_password = os.environ.get("DB_PASSWORD", "")
+    db_name = os.environ.get("DB_NAME", "")
+    os.environ["DATABASE_URL"] = f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
